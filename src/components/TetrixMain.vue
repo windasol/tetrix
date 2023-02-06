32f75bfa8018e5330ae5d37354df3148725438e5
Shin dongmin <sdm77>
2023-02-06 12:28:46 +0900

    수정

--- src/components/TetrixMain.vue
+++ src/components/TetrixMain.vue
@@ -45,11 +45,10 @@ export default {
   },
   data() {
     return {            
-      now: [],      
-      count: 0,      
-      stackData: [],
-      blocks: [4, 4, 2, 2, 0 ],
+      now: [],                    
       flag: true,
+      down: false,
+      time: 100,
     };
   },
   methods: {
@@ -58,13 +57,9 @@ export default {
     },
     // 블럭 생성
     async makeBlock() {            
+      this.down = false;
 
-      // if(this.count == 5) {
-      //   return 0;
-      // }      
-      
       let num = Math.floor(Math.random() * 7);               
-      // let num = this.blocks[this.count];
       let block = ''; //eslint-disable-line no-unused-vars                  
       let color = this.colorSelect(num);            
 
@@ -75,9 +70,9 @@ export default {
           document.getElementById(block).style.backgroundColor = color; 
       }         
       
-
-      const stackMax = Math.max(...this.now.map(e => e[0]).flat());      
-      let result = this.stackCheck(this.now.filter(e => e[0] == stackMax).map(e => [e[0] + 1, e[1]]));
+      // 밑에 블럭 있는지 체크
+      let check = this.blockLimitArr(this.now.map(e=> [e[0] + 1, e[1]]));
+      let result = this.stackCheck(check);
 
       if(result == "return") {        
         return ;
@@ -116,46 +111,21 @@ export default {
     },
     // 자동으로 움직이는함수
     async autoMove(now, color) {          
-      
+            
       // 떨어지는 시간 동기처리
-      await this.delay(10);
+      await this.delay(this.time);      
                                                                                             
       const stackMax = Math.max(...this.now.map(e => e[0]).flat());       
       
       if(stackMax == 20) {          
         this.flag = false;
         return;
-      }
-
-
-      let plus = this.now.map(e=> [e[0] + 1, e[1]]);
-      let dd = this.now.map(e=> [e[0], e[1]]);
-      // console.log(dd[1]);              
-      // console.log(plus[0]);    
-      // console.log(dd[1].equals(plus[0]));
-      console.log(JSON.stringify(plus[0] == JSON.stringify(dd[1])));
-      let ss = [];
-
-      // console.log(plus[0].includes(this.now[0]));
-
-      for(let i = 0; i < plus.length; i++) {
-        for(let j = 0; j < plus.length; j++) {
-          if(plus[i] == dd[j]) {
-            alert("sdfsd");
-            break;
-          }
-
-          if(j == 3) {
-            ss.push("sdfdsf : " + plus[i]);
-          }          
-        }
-      }
-      console.log(ss);
-      ss = [];
+      }    
 
       //밑에 블럭이 있는지 계산
-      let result = this.stackCheck(this.now.filter(e => e[0] == stackMax).map(e => [e[0] + 1, e[1]]));
-            
+      let check = this.blockLimitArr(this.now.map(e=> [e[0] + 1, e[1]]));
+      let result = this.stackCheck(check);      
+                        
       // 밑에 블럭이 있을때
       if(result == "return") {
         this.now = this.now.map(e=> [e[0] + 1, e[1]]);                      
@@ -169,7 +139,11 @@ export default {
       this.colorStack(color);
     },
     // 떨어지는 속도
-    delay(ms) {
+    delay(ms) {      
+      if(this.down) {
+        ms = 0;        
+      }
+      
       return new Promise(r => setTimeout(r,ms));
     }, 
     // 블럭 쌓기   
@@ -180,8 +154,8 @@ export default {
         document.getElementById(block).style.backgroundColor = color; 
       }   
     },    
-    // 밑에 블럭있는지 체크
-    stackCheck(check) {            
+    // 블럭있는지 체크
+    stackCheck(check) {                       
       let block = '';                  
       for(let i = 0; i < check.length; i++) {                             
         block = check[i].toString();             
@@ -195,23 +169,59 @@ export default {
     },
     keydownEvent(event) {
       if(event.keyCode == 38) {
+
+
         // alert("윗키");
-      } else if(event.keyCode == 37) {
-        if(this.now.filter(e=> e[1] == 1).length > 0) {
+      } else if(event.keyCode == 37) {        
+        const stackMin = Math.min(...this.now.map(e => e[1]).flat());              
+        if(stackMin - 1 == 0) {
             return;
         }                
+        
+        let result = this.stackCheck(this.blockLimitArr(this.now.map(e=> [e[0], e[1] - 1])));
+
+        if(result == "return") {
+          return;
+        }                
+
         this.colorStack("green");
-        this.now = this.now.map(e => [e[0], e[1] - 1]);        
-      } else if(event.keyCode == 39) {
-        if(this.now.filter(e=> e[1] == 10).length > 0) {
+        this.now = this.now.map(e => [e[0], e[1] - 1]);                
+      } else if(event.keyCode == 39) {        
+        const stackMax = Math.max(...this.now.map(e => e[1]).flat());              
+        if(stackMax == 10) {
             return;
         }                
+        
+        let result = this.stackCheck(this.blockLimitArr(this.now.map(e=> [e[0], e[1] + 1])));
+
+        if(result == "return") {
+          return;
+        }            
         this.colorStack("green");
         this.now = this.now.map(e => [e[0], e[1] + 1]);               
-      } else if(event.keyCode == 40) {
-        // alert("아랫키");
+      } else if(event.keyCode == 40) {                
+        this.down = true;
       }  
-    },    
+    },
+    // 
+    blockLimitArr(arr) {      
+      let arr2 = this.now.map(e=> [e[0], e[1]]);      
+      let compare = [];
+      
+      for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arr.length; j++) {
+          if(JSON.stringify(arr[i]) == JSON.stringify(arr2[j])) {            
+            break;
+          }
+
+          if(j == 3) {
+            compare.push(arr[i]);
+          }          
+        }
+      }     
+      
+      return compare;
+    }
   },
 }
 </script>
